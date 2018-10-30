2dd37ff3e0bd7323d12972526ab7e6b8fb69ef83
Giorgio Audino <gioaudino91@gmail.com>
2018-10-30 15:09:45 +0100

    Created classes to evaluate cost for a window with both bounds

---
+++ src/com/gioaudino/thesis/DoubleBoundApproximatedPartition.java
@@ -0,0 +1,80 @@
+package com.gioaudino.thesis;
+
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DoubleBoundApproximatedPartition {
+
+    final static double EPS_1 = 0.03;
+    final static double EPS_2 = 0.3;
+
+    private int[] nodes;
+    private int size;
+    private IntArrayList partition;
+    private long cost;
+
+    public DoubleBoundApproximatedPartition(int[] nodes) {
+        this.nodes = nodes;
+        this.size = nodes.length;
+    }
+
+    public IntArrayList createDoubleBoundApproximatedPartition() {
+        final long singleBlockCost = CostEvaluation.evaluateCost(this.nodes[this.nodes.length - 1] - this.nodes[0] + 1, this.size, true); //this.evaluateCost(this.nodes[this.nodes.length - 1], size);
+        List<DoubleBoundWindow> windows = new ArrayList<>();
+        long minimumCost = CostEvaluation.evaluateCost(1, 1, true);
+        long costBound = minimumCost;
+        while (costBound < minimumCost / EPS_1) {
+            windows.add(new DoubleBoundWindow(nodes, costBound));
+            if (costBound > singleBlockCost) break;
+            costBound *= 1 + EPS_2;
+        }
+
+        StepInPath[] steps = new StepInPath[size + 1];
+        for (int i = 0; i < steps.length; i++) {
+            steps[i] = new StepInPath();
+            steps[i].weight = singleBlockCost;
+            steps[i].from = 0;
+        }
+        steps[0].from = null;
+        steps[0].weight = 0L;
+
+        for (int i = 0; i < this.size; i++) {
+            int lastEnd = i + 1;
+            for (DoubleBoundWindow window : windows) {
+                while (window.end < lastEnd)
+                    window.advanceEnd();
+
+                while (true) {
+                    long windowCost = window.getCost();
+                    if (steps[window.end].weight > steps[i].weight + windowCost) {
+                        steps[window.end].weight = steps[i].weight + windowCost;
+                        steps[window.end].from = i;
+                    }
+                    lastEnd = window.end;
+                    if (window.end == size || windowCost >= window.maxCost) break;
+                    window.advanceEnd();
+                }
+                window.advanceStart();
+            }
+        }
+        int n = steps.length - 1;
+        this.partition = new IntArrayList();
+        while (true) {
+            this.partition.add(0, n);
+            if (steps[n].from == null) break;
+            n = steps[n].from;
+
+        }
+        this.cost = steps[steps.length - 1].weight;
+
+        return this.partition;
+
+    }
+
+    long getCost() {
+        return cost;
+    }
+
+}
